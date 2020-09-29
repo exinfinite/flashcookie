837a1c0ece5a0eedac9d001938b3275f49de8be2
Exinfinite chen <chen.min.home@gmail.com>
2020-09-29 16:17:24 +0800

    add:urlencode

--- src/FlashCookie.php
+++ src/FlashCookie.php
@@ -4,16 +4,16 @@ namespace Exinfinite;
 class FlashCookie {
     const KEY = "_notify_msg";
     static function set($msg, $status = 'success') {
-        setcookie(self::KEY, serialize([
+        setcookie(self::KEY, urlencode(serialize([
             'msg' => $msg,
             'status' => $status,
-        ]));
+        ])));
     }
     static function get() {
         if (!array_key_exists(self::KEY, $_COOKIE)) {
             return false;
         }
-        $data = unserialize($_COOKIE[self::KEY]);
+        $data = unserialize(urldecode($_COOKIE[self::KEY]));
         setcookie(self::KEY, '');
         return $data;
     }
